c976b402354fc3daf50c0d02884b2cc54ea70e5e
Shan Tirmizi <tirmizishahnawaz@gmail.com>
2023-11-09 16:39:54 +0000

    Added the ability to authenticate

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,23 @@
+import NextAuth, { AuthOptions } from "next-auth"
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import prisma from '../../../../../prisma/client'
+
+export const authOptions: AuthOptions =  {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID ?? "",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET ?? "",
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+  ],
+}
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
